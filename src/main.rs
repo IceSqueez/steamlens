add493fef5378433b721f87bac4f691b20d3e295
IceSqueez <IceSqueez@gmail.com>
2025-11-20 02:08:50 +0200

    feat(ui): added basic ui for

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
-fn main() {
-    println!("Hello, world!");
+use std::path::PathBuf;
+
+use iced::{
+    Element, Task,
+    widget::{button, column, row, text},
+    window,
+};
+
+#[derive(Debug)]
+struct AppState {
+    current_dir: PathBuf,
+}
+
+impl Default for AppState {
+    fn default() -> Self {
+        AppState {
+            current_dir: std::env::current_dir().unwrap(),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+enum Message {
+    Exit,
+}
+
+fn update(state: &mut AppState, message: Message) -> Task<Message> {
+    match message {
+        Message::Exit => window::get_latest().and_then(window::close),
+    }
+}
+
+fn view(state: &AppState) -> Element<'_, Message> {
+    column![
+        row![
+            text(state.current_dir.to_str().unwrap_or("unknown dir")).size(24),
+            button(text("Exit").size(24)).on_press(Message::Exit),
+            button(text("Up").size(24)).on_press(Message::Exit)
+        ]
+        .spacing(8)
+    ]
+    .into()
+}
+
+fn main() -> iced::Result {
+    iced::application("SteamLens", update, view)
+        .theme(|_x| iced::Theme::Dracula)
+        .run()
 }
